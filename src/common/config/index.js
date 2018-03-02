fe6616533970c9d2e6ecb8c2919144548cfcab04
陈林 <chenlin@chenlindeMBP.lan>
2018-03-02 16:03:28 +0800

    1

--- src/common/config/index.js
+++ src/common/config/index.js
@@ -9,7 +9,7 @@
  */
 const isTest = location.host.match(/36.7|localhost|21.80/) != null
 //本地存储的前缀
-export const storage_prefix = 'zzm_admin_storage_'
+export const storage_prefix = 'zzm_admin_storage_';
 //base64加密前缀
 export const base64_prefix = 'zzm_admin_base64_';
 //接入服务器接口地址根目录
